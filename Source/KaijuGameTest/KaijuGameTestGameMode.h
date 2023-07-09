c9a943b8db7dca3d93bcd81ebab6abbc77b44678
Hage <89595749+HageFX-78@users.noreply.github.com>
2023-07-10 03:30:05 +0800

    Commit def files again

---
+++ Source/KaijuGameTest/KaijuGameTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KaijuGameTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AKaijuGameTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AKaijuGameTestGameMode();
+};
+
+
+
